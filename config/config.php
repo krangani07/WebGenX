2f5843ee81607f778d43016f6744c16007e444ae
Kaushal Rangani <krangani293@rku.ac.in>
2025-07-28 19:15:33 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@ ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../logs/debug.log');
 error_reporting(E_ALL);
 
-define('API_KEY', 'AIzaSyAJgHYnjxUX-V1bmJBosHSqnRPdjbuv-PU');
+define('API_KEY', 'Your Api here');
 define('API_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-thinking-exp-01-21:generateContent');
 
 
-define('USER_WEBSITES', 'c:/xampp/htdocs/qp/WebGenX/tests');
+define('USER_WEBSITES', 'c:/xampp/htdocs/');
